8f80a00585fc07c7ca36ccfe410322711b712458
Marta315 <martagraziano02@gmail.com>
2024-11-27 20:03:24 +0100

    Add files via upload

--- Sis_Congruenze.c
+++ Sis_Congruenze.c
@@ -2,34 +2,44 @@
 #include <stdlib.h>
 #include <stdbool.h>
 #include "Smith.h"
+#include "Bezout.h"
 
 int** input_sys (int**, int);
-void print_sys(int**, int);
+void print_sys (int**, int);
 int* input_vec (int*, int);
 int* input_vec_one (int*, int);
 void solve_sys (int**, int*, int*, int*, int*, int);
+int adjust_sol (int, int);
 
 int main() {
 	int m;
 	printf("Numero di equazioni nel sistema: ");
 	scanf("%d", &m);
 	printf("Dati del sistema (ad x=a mod b corrisponde l'input da tastiera 'a b'):\n"); 
-	int **sys=input_sys(sys, m);
+	int **sys=NULL;
+	sys=input_sys(sys, m);
 	print_sys(sys, m);
 	
 	// vettori necessari per la risoluzione del sistema 
-	int *A=input_vec(A, m);
-	int *B=input_vec_one(B, m);
-	int *Alpha=input_vec(Alpha, m);
-	int *Gamma=input_vec(Gamma, m);	
+	int *A=NULL; 
+	A=input_vec(A, m);
+	int *B=NULL; 
+	B=input_vec_one(B, m);
+	int *Alpha=NULL; 
+	Alpha=input_vec(Alpha, m);
+	int *Gamma=NULL; 
+	Gamma=input_vec(Gamma, m);	
 	solve_sys(sys, A, B, Alpha, Gamma, m);
 	
 	// troviamo la soluzione x
-	int x=0, mod=1;
+	int x=0; 
+	int mod=1;
 	for (int i=0; i<m; i++) {
 		x=x+Gamma[i]*B[i]*Alpha[i];
 		mod=mod*A[i];
 	}
+	// aggiustiamo la soluzione x rendendola della forma x=a mod b con 0<=a<b
+	x=adjust_sol(x, mod);
 	printf ("Soluzione: x=%d mod %d\n", x, mod);
 	
 	return 0; 
@@ -89,15 +99,37 @@ void solve_sys (int **sys, int *A, int *B, int *Alpha, int *Gamma, int m) {
 		Alpha[i]=sys[i][0]; // Alpha è vettore delle alpha 
 	}
 	
-	int **D=input_null(D, m, 1);
+	int **D=NULL;
+	D=input_null(D, m, 1);
+	
+	
 	for (int i=0; i<m; i++) {
 		D[i][0]=B[i]; // inizializziamo D a B
 	}
-	int **S=input_id(S, m);
-	int **T=input_id(T, 1);
+	
+	int **S=NULL; 
+	S=input_id(S, m);
+	int **T=NULL; 
+	T=input_id(T, 1);
 	SmithNormalForm(D, S, T, m, 1); // applichiamo Smith alla matrice/vettore D
+	//bezout (m, B, D, S, T);
+	
 	for (int i=0; i<m; i++)  {
 		Gamma[i]=S[0][i]; // Gamma è la prima riga della matrice S prodotta da Smith(D) (corrisponde a D*S essendo D del tipo (1, 0, ..., 0)
 	}
 	return;
 }
+
+// aggiustiamo la soluzione x rendendola della forma x=a mod b con 0<=a<b
+int adjust_sol (int x, int mod) {
+	if (abs(x)>=mod) { // se abs(x)>=mod togliamo ad x i multipli di mod perchè a*x=x mod a
+		int temp=0;  
+		temp=x/mod;
+		x=x-temp*mod;
+	}
+	if (x<0) { // se x<0 prendiamo la sua classe equivalente positiva
+		x=mod+x; 
+	}
+	return x;
+}
+		
