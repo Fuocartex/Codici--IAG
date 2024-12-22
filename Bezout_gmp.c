555382f4720c1c646cdda2830216369839c77dc8
giacomogneri <giacomo.gneri7@gmail.com>
2024-12-22 19:30:14 +0100

    Aggiunto mcd con libreria gmp

--- Bezout_gmp.c
+++ Bezout_gmp.c
@@ -1,17 +1,19 @@
 #include <stdio.h>
-#include "Bezout_gmp.h"
+#include "mcd.h"
 
 int main () {
-    mpz_t a,b;
+    mpz_t a,b,x;
     mpz_init(a);
     mpz_init(b);
+    mpz_init(x);
     gmp_scanf("%Zd",a);
     gmp_scanf("%Zd",b);
 
-    MCD(a,b);
-    gmp_printf("%Zd",a);
+    mcd_binario(x,a,b);
+    gmp_printf("%Zd",x);
 
     mpz_clear(a);
     mpz_clear(b);
+    mpz_clear(x);
     return 0;
 }
