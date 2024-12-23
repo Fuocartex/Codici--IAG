cf53be58b6c811a18ac09c0b7385473301d7fa51
Marta315 <martagraziano02@gmail.com>
2024-12-23 18:36:22 +0100

    Add files via upload

--- algebra/Smith.c
+++ algebra/Smith.c
@@ -17,24 +17,32 @@ int main() {
 	S=input_id(S, m);
 	int **T=NULL;
 	T=input_id(T, n);
+	int **S_inv=NULL;
+	S_inv=input_id(S_inv, m);
+	int **T_inv=NULL;
+	T_inv=input_id(T_inv, n);
 	
 	if (nullMatrix(A, m, n)) {
 		printf("La matrice in esame è nulla\n");
 		return 0;
 	}
 	
-	SmithNormalForm(D, S, T, m, n);
+	SmithNormalForm5mat(D, S, T, S_inv, T_inv, m, n);
 	
 	printf("\nmat A = ");
-	printMatrix(A, m, n);
+	printMatrix(A, m, n, 8);
 	printf("\nmat D = ");
-	printMatrix(D, m, n);
+	printMatrix(D, m, n, 8);
 	printf("\nmat S = ");
-	printMatrix(S, m, m);
+	printMatrix(S, m, m, 8);
 	printf("\nmat T = ");
-	printMatrix(T, n, n);
+	printMatrix(T, n, n, 8);
+	printf("\nmat S_inv = ");
+	printMatrix(S_inv, m, m, 12);
+	printf("\nmat T_inv = ");
+	printMatrix(T_inv, n, n, 12);
 	
-	// verifichiamo che la funzione Smith funzioni correttamente vericando che S*A*T=D
+	// verifichiamo che la funzione Smith funzioni correttamente vericando che S*A*T=D e S_inv*D*T_inv=A
 	int **SA=NULL; 
 	SA=input_null(SA, m, n);
 	int **SAT=NULL;
@@ -42,7 +50,16 @@ int main() {
 	multiplyMatrices(S, A, SA, m, m, n);
 	multiplyMatrices(SA, T, SAT, m, n, n); 
 	printf("\nS*A*T = ");
-	printMatrix(SAT, m, n);
+	printMatrix(SAT, m, n, 8);
+	
+	int **SinvD=NULL; 
+	SinvD=input_null(SinvD, m, n);
+	int **SinvDTinv=NULL;
+	SinvDTinv=input_null(SinvDTinv, m, n);
+	multiplyMatrices(S_inv, D, SinvD, m, m, n);
+	multiplyMatrices(SinvD, T_inv, SinvDTinv, m, n, n); 
+	printf("\nS_inv*D*T_inv = ");
+	printMatrix(SinvDTinv, m, n, 16);
 	
 	return 0; 
 }
