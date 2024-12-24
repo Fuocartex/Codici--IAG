1b3613b39645986b7fafb2306af4e5ea6198fc01
IlTuoNome <laTuaEmail@example.com>
2024-12-24 09:52:41 +0100

    a

--- geometria/edge_matrix.c
+++ geometria/edge_matrix.c
@@ -2,18 +2,25 @@
 
 int main(){
 	
-    	int size = 0;
+    int size = 0, n = 0;
 	printf("Inserisci la grandezza del simplesso piu' grande nel complesso: ");
-    	scanf("%d", &size);
+    scanf("%d", &size);
     	
-    	SimplicialComplex* complex = readComplex(size);
+    SimplicialComplex* complex = readComplex(size);
     	
-    	if(!isSimplicial(complex, size)){
-    		printf("Errore il simplesso non è simpliciale inserire un nuovo simplesso\n");
-    		return 1;
+    if(!isSimplicial(complex, size)){
+    	printf("Errore il simplesso non e' simpliciale inserire un nuovo simplesso\n");
+    	return 1;
 	}
 	
-    	printComplex(complex, size);
+	printf("Inserisci il numero n di cui vuoi calcolare la matrice di bordo n -> n-1\n n > 0 = ");
+	scanf("%d", &n);
+	if (!n) {
+		printf("Errore n deve essere maggiore di 0\n");
+		return 1;
+	}
+
+    printComplex(complex, size);
 	return 0;
 }
 
