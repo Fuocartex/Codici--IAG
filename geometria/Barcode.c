075eec093a3069e73ba12b0be27350d807b1fa9e
Fuocartex <bonanni.1992505@studenti.uniroma1.it>
2025-02-16 23:21:46 +0100

    aggiunti commenti

--- geometria/Barcode.c
+++ geometria/Barcode.c
@@ -1,17 +1,28 @@
 #include "..\Include\Barcode.h"
 
+//dati i punti su un piano cartesiano studiamone la topologia attraverrso il barcode
+
 int main()
 {
     int** m = NULL;
 	int* k = 0;
 	double* l_max = malloc(sizeof(double));
+	//salvo in m i punti di un piano cartesiano di cui voglio studiare la topologia
 	m = input_point(&k);
-	int n = k;
+	int n = k, h = 0, max = 0;
     double** d = NULL;
 	
+	//calcolo la matrice delle distanze
     d = distance_matrix(m, n);
 	
-	printf("Matrice delle distanze:\n");
+	printf("Quale omologia vuoi calcolare? h = ");
+	scanf("%d", &h);
+
+	printf("Usando la matrice delle distanze si ottengono le H > 0 tutte nulle fino a quale k vuoi calcolare i complessi simpliciali? max = ");
+	scanf("%d", &max);
+
+	//stampo la matrice delle distanze
+	printf("\n\nMatrice delle distanze:\n");
 	for (int i = 0; i < n; i++)
 	{
 		for (int j = 0; j < n; j++)
@@ -20,37 +31,33 @@ int main()
 		}
 		printf("\n");
 	}
+
+	//creo la successione di matrici partendo dalla matrice delle distanze
 	matrici_persistenza* mp = NULL;
 	mp = create_matrix_persistenza(d, n, l_max);
 
-	//matrici_persistenza* mp = NULL;
-	//mp = create_M_P();
-	M_P* mod_p = create_Modulo_Persistenza(mp);
-	//print1(mod_p);
+	//creo il modulo di persistenza ovvero l'evoluzione dei complessi simpliciali
+	M_P* mod_p = create_Modulo_Persistenza(mp, max);
+	print1(mod_p, max);
 
-	//int h = beta_i_j(mod_p, 11.5, 11.5, 0, 1);
-
-	//printf("beta_i_j = %d\n", h);
-
+	//calcolo la matrice beta essendomi calcolato a mano la distanza più grande tra i punti e aggiungo 2
 	int** matrix = NULL;
-	//printf("l_max = %lf\n", l_max[0]);
 	int lambda = ceil(l_max[0]);
-	//printf("l_max = %d\n", lambda);
-	matrix = beta_matrix(0, lambda, 0.5, mod_p, 1, 1);
+	matrix = beta_matrix(0, lambda, 0.5, mod_p, h, max);
 	int dim = lambda / 0.5 + 2;
-	//print_matrix(matrix, dim, dim);
 
+	//calcolo la matrice mu
 	int** mu = NULL;
 	mu = mu_matrix(matrix, dim);
 	//printf("mu\n");
 	//print_matrix(mu, dim, dim);
 	//printf("beta_i_j = %d\n", h);
 
+	//da qui passo la matrice ad un file python che mi stampa il barcode molto più efficiente di c in questo
+	//gli passo la matrice come un file json ovvero di caratteri
 	char* json_matrix = matrix_to_json(mu, dim);
     
-	//printf("Matrice in formato JSON: %s\n", json_matrix);
-
-    n = dim;  // Supponiamo che la matrice sia 100x100
+    n = dim;  
     int matrix_size = n * n * 4; // Stima della memoria necessaria (interi in JSON)
     int command_size = matrix_size + 100; // Qualche byte extra per il comando
 
@@ -60,6 +67,7 @@ int main()
         return 1;
     }
 
+	//scrivo il comando 
     snprintf(command, command_size, "python barcode1.py %s %lf %lf", json_matrix, 0, 0.5);
 
     //printf("Eseguo il comando: %s\n", command);
@@ -72,11 +80,11 @@ int main()
     }
 
     // Pulizia: liberiamo la memoria allocata
-    /*free(json_matrix);
+    free(json_matrix);
     for (int i = 0; i < 24; i++) {
         free(matrix[i]);
     }
-    free(matrix);*/
+    free(matrix);
 	
 	return 0;
 }
